01f50e9952f048fc92a108667a678168173d4f2b
Diego Andrade <dcandrade19@gmail.com>
2018-04-16 15:34:39 -0300

    delete teste

--- versao_teste/SistemaADMC/model/SQL/sql_Condominios.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists condominios(
-	id int(11) not null AUTO_INCREMENT,
-    nome varchar(255) not null,
-    endereco varchar(255) not null,
-    status int(1) not null,
-    
-    PRIMARY KEY(id)   
-);
--- versao_teste/SistemaADMC/model/SQL/sql_Funcoes.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists funcoes(
-	id int(11) not null AUTO_INCREMENT,
-    nome varchar(255) not null,
-    id_condominio int,
-    
-    PRIMARY KEY(id),
-    FOREIGN KEY (id_condominio) REFERENCES condominios(id)
-);
